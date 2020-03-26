3fcf8a8549d7cee028c8f5663483b87eab8b2ef6
chenms <frankchen1328@gmail.com>
2020-03-26 11:36:40 +0800

    第27 题 RemoveElement

---
+++ src/main/java/_27_removeElement/RemoveElement.java
@@ -0,0 +1,48 @@
+package _27_removeElement;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 3, 2, 2, 3 };
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int[] array = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                array[index++] = nums[i];
+            }
+        }
+        nums = Arrays.copyOfRange(array, 0, index);
+        return nums.length;
+    }
+
+    public static int removeElement2(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        nums = Arrays.copyOfRange(nums, 0, index);
+        return nums.length;
+    }
+
+    /**
+     * 不做copy，直接返回结果
+     */
+    public static int removeElement3(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
